82a74892a56142f99eb5e5d9a2930421aea39e6e
lynksreich <marcelinseble@gmail.com>
2020-02-04 05:14:26 +0100

    Update Startup file

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace asp_mariadb_cfg
         {
             services.AddControllers();
             services.AddDbContext<AMCDbContext>(options => options
-                .UseMySql("Server=localhost;Database=asp_mariadb_cfg;User=<username>;Password=<password>;",
+                .UseMySql("Server=localhost;Database=asp_mariadb_cfg;User=<Username>;Password=<Password>;",
                     mysqlOptions =>
                         mysqlOptions.ServerVersion(new ServerVersion(new Version(10, 4, 6), ServerType.MariaDb))));
         }
@@ -46,7 +46,7 @@ namespace asp_mariadb_cfg
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();       
+                endpoints.MapControllers();
             });
         }
     }
